0db66c61bca73183716292f65dce15860308c79e
LapisMYT <nikitagavrilin005@gmail.com>
2023-10-30 21:08:34 +0300

    .

---
+++ main.py
@@ -0,0 +1,29 @@
+from bedrock.server import Server
+
+app = Server()
+
+@app.server_event
+async def ready(ctx):
+    print(f"Готово @ {ctx.host}:{ctx.port}!")
+
+@app.game_event
+async def block_broken(ctx):
+    await ctx.server.run(f"say Сломан блок {ctx.id} игроком {ctx.player}")
+
+@app.game_event
+async def item_used(ctx);
+    await ctx.server.run(f"say Использовали предмет (мне лень писать какой)")
+
+@app.game_event
+async def portal_built(ctx):
+    await ctx.server.run(f"say Портал был построен")
+
+@app.game_event
+async def player_message(ctx):
+    await ctx.server.run(f"say Сообщение отправлено игроком {ctx.sender}")
+
+@app.game_event
+async def item_dropped(ctx):
+    await ctx.server.run(f"say Уронили предмет. Не мыло(")
+
+app.start("lapismyt.space", 6464)
